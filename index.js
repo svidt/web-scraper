ae06bdf9e66dd68b7d3e0a8ca8cef54727887823
Svidt <79602784+svidt@users.noreply.github.com>
2022-10-09 15:55:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,35 +6,32 @@ const express = require('express')
 
 const app = express()
 
-const url = 'https://kristianemil.dk/contact'
+const url = 'https://politiken.dk'
 
 axios(url)
     .then(response => {
         const html = response.data
         const $ = cheerio.load(html)
-        const contact = []
+        const articles = []
 
-        $('.image-caption', html).each(function() {
-            const info = $(this).text()
-            contact.push({
-                info
+        $('.article-intro__title', html).each(function() {
+            const headline = $(this).text().trim('\n')
+            const url = $(this).find('a').attr('href')
+            articles.push({
+                headline,
+                url
             })
         })
 
-        $('.image-caption', html).each(function() {
-            const links = $(this).find('a').attr('href')
-            contact.push({
-                links
+        $('.footer__col-item', html).each(function() {
+            const title = $(this).find('h2').text().trim('\n')
+            const person = $(this).find('p').text().trim('\n')
+            articles.push({
+                title,
+                person
             })
         })
-
-        $('#block-179f8c9da7aa428c1198', html).each(function() {
-            const footer = $(this).text().trim('\n')
-            contact.push({
-                footer
-            })
-        })
-        console.log(contact)
+        console.log(articles)
     }).catch(err => console.log(err))
 
 app.listen(PORT, () => console.log('server running on PORT ${PORT}'))
